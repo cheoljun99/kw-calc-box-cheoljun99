ff09be9891ffbb7d6d37f681fc6a1f6feab96337
cheoljun99 <cheoljun99@naver.com>
2023-11-01 22:23:49 +0900

    [2018202065/박철준] : Assignment1 최종 업로드

--- lib.test.js
+++ lib.test.js
@@ -8,3 +8,15 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg([-5, 5]) should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+test("prime([17]) should be true", () => {
+    expect(lib.prime([17])).toBe(true);
+});
+test("prime([6]) should be false", () => {
+    expect(lib.prime([6])).toBe(false);
+});
+test("fact([15]) should be 1307674368000", () => {
+    expect(lib.fact([15])).toBe(1307674368000);
+});
+test("fact([5]) should be 120", () => {
+    expect(lib.fact([5])).toBe(120);
+});
